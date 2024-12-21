9496d2ff73913afe3e7da1bb07c110602399eb48
Rafael Freire <rafaeldacostafreire@gmail.com>
2024-12-21 13:35:54 -0300

    build: :heavy_plus_sign: new 'uuid' lib added

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -17,6 +17,7 @@ import {
   RoomClient,
 } from "../types/game.types";
 import { Logger, OnModuleInit } from "@nestjs/common";
+import { v4 as uuidv4 } from "uuid";
 
 @WebSocketGateway()
 export class GameGateway implements OnModuleInit {
